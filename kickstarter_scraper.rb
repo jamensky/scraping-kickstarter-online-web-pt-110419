9ec1b29b9a9213862f5d73d45a446a42e1945e2a
Jamal Rjoul <jamensky1991@gmail.com>
2020-01-10 02:21:46 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,7 +8,7 @@ def create_project_hash
   project = kickstarter.css('li.project.grid_4')
   title = project.css('h2.bbcard_name strong a').text
   image = project.css('div.project-thumbnail a img').attribute('src').value
-  #description = project.('p.bbcard_blurb').text
+  description = project.css('p.bbcard_blurb').text
 
   binding.pry
 end
