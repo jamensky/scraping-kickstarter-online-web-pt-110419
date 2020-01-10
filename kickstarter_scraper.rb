90c0fec002758bbc2c167e029a8a42364ba37979
Jamal Rjoul <jamensky1991@gmail.com>
2020-01-10 02:32:07 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,8 +10,7 @@ def create_project_hash
   image = project.css('div.project-thumbnail a img').attribute('src').value
   description = project.css('p.bbcard_blurb').text
   location = project.css('span.location-name').text
-  percent_funded = project.css('ul.peoject-stats li.first.funded strong').text
-  binding.pry
+  percent_funded = project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i  binding.pry
 end
 
 create_project_hash
