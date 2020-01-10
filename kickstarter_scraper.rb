5753c920e7b49189d165c0635b6d024629146e26
Jamal Rjoul <jamensky1991@gmail.com>
2020-01-10 02:05:07 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,7 +5,7 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
 
-  x = kickstarter.css('li.project.grid_4').first
+  x = kickstarter.css('li.project.grid_4').first.css("h2.bbcard_name strong a").text
 
   binding.pry
 end
