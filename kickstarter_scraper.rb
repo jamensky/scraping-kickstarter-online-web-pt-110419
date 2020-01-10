2568fa433408b8490c5aab5b10dc03321d6cd5ed
Jamal Rjoul <jamensky1991@gmail.com>
2020-01-10 02:34:14 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,16 +1,18 @@
 require 'nokogiri'
 require 'pry'
 
+#project = kickstarter.css('li.project.grid_4')
+#title = project.css('h2.bbcard_name strong a').text
+#image = project.css('div.project-thumbnail a img').attribute('src').value
+#description = project.css('p.bbcard_blurb').text
+#location = project.css('span.location-name').text
+#percent_funded = project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i  binding.pry
+
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
 
- #project = kickstarter.css('li.project.grid_4')
- #title = project.css('h2.bbcard_name strong a').text
- #image = project.css('div.project-thumbnail a img').attribute('src').value
- #description = project.css('p.bbcard_blurb').text
- #location = project.css('span.location-name').text
- #percent_funded = project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i  binding.pry
+ 
 end
 
 create_project_hash
